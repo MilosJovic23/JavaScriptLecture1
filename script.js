b9a778922fcf45eaf2e26c7253ae5bf89d931c9b
MilosJovic23 <milos.mj992@gmail.com>
2024-06-02 15:36:58 +0200

    * initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+var mojeIme = "Milos"; // string -text- "   "  //
+var godine = 31; // int -broj //
+var ime, prezime, ispis;
+ime = "Milos";
+prezime = "Jovic";
+ispis = ime + prezime;
+var visina = 190.2; // float - decimal//
+var mentor = true; //bool- boolean - true -false (1 ili 0)//
+
+console.log(mojeIme, godine, visina, mentor, ispis);
---
+++ vezba.js
@@ -0,0 +1,5 @@
+var mojeIme = "Milos";
+var designer = true;
+var godineIskustva = 9;
+
+console.log(mojeIme, designer, godineIskustva);
