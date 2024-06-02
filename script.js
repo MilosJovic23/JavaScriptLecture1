1525fa6e3b4e13aa825687715f12b71a7bc03726
MilosJovic23 <milos.mj992@gmail.com>
2024-06-02 15:38:18 +0200

    * small changes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("Hello World");
 
-var mojeIme = "Milos"; // string -text- "   "  //
-var godine = 31; // int -broj //
-var ime, prezime, ispis;
+let mojeIme = "Milos"; // string -text- "   "  //
+let godine = 31; // int -broj //
+let ime, prezime, ispis;
 ime = "Milos";
 prezime = "Jovic";
 ispis = ime + prezime;
-var visina = 190.2; // float - decimal//
-var mentor = true; //bool- boolean - true -false (1 ili 0)//
+let visina = 190.2; // float - decimal//
+let mentor = true; //bool- boolean - true -false (1 ili 0)//
 
 console.log(mojeIme, godine, visina, mentor, ispis);
